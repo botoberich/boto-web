fb5ea8687e88052886c2b3600431dc939127c234
Boto <botoberich@gmail.com>
2019-08-03 10:52:39 -0400

    Test cors change

--- server/src/index.js
+++ server/src/index.js
@@ -8,31 +8,29 @@ const app = express();
 const PORT = process.env.DEBUG ? 4001 : 4000;
 const Cache = new NodeCache();
 
-const whitelist = ['http://boto.photos', 'https://boto.photos'];
-const corsOptions = {
-    origin: function(origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-};
-
 app.use(
-    cors(corsOptions),
+    cors({
+        origin: [/boto\.photos$/, '/localhost/'],
+    }),
     bodyParser.json({
         limit: '50mb',
     })
 );
+
+app.get('/status/healthcheck', (req, res) => {
+    res.status(200).json({ status: 'Healthy' });
+});
+
 app.use('/get/:id', (req, res) => {
     let cache = Cache.get(req.params.id);
     res.json({ status: 'success', cache });
 });
+
 app.post('/set', (req, res) => {
     Cache.set(req.body.id, req.body.cache);
     res.json({ status: 'success' });
 });
+
 app.listen(PORT, async () => {
     console.log(`EXPRESS SERVER LISTENING ON PORT ${PORT}!`);
 
