50cdda018f3facc943be36a52746764f104d13e6
Boto <botoberich@gmail.com>
2019-08-20 01:35:36 -0400

    Sort days

--- web/src/app/components/PhotoGrid/PhotoGrid.tsx
+++ web/src/app/components/PhotoGrid/PhotoGrid.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { format, isToday } from 'date-fns';
+import { format, isToday, compareDesc } from 'date-fns';
 
 // UI
 import { Skeleton, notification, Typography } from 'antd';
@@ -54,23 +54,29 @@ function PhotoGrid() {
 
     return (
         <div className={styles.gridContainer}>
-            {Object.keys(thumbnails).map(date => {
-                return (
-                    <div key={date}>
-                        <Title level={3}>{isToday(date) ? 'Today' : format(date, 'D MMM YYYY')}</Title>
-                        <div className={styles.grid}>
-                            {thumbnails[date].map(({ b64, photoId }) => {
-                                if (!b64) {
-                                    return <Skeleton key={photoId} active />;
-                                }
-                                return (
-                                    <PhotoGridItem id={photoId} key={photoId} src={`data:image/png;base64,${b64}`} />
-                                );
-                            })}
+            {Object.keys(thumbnails)
+                .sort(compareDesc)
+                .map(date => {
+                    return (
+                        <div key={date}>
+                            <Title level={3}>{isToday(date) ? 'Today' : format(date, 'D MMM YYYY')}</Title>
+                            <div className={styles.grid}>
+                                {thumbnails[date].map(({ b64, photoId }) => {
+                                    if (!b64) {
+                                        return <Skeleton key={photoId} active />;
+                                    }
+                                    return (
+                                        <PhotoGridItem
+                                            id={photoId}
+                                            key={photoId}
+                                            src={`data:image/png;base64,${b64}`}
+                                        />
+                                    );
+                                })}
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
         </div>
     );
 }
