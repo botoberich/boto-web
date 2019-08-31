c4716545c83d73d9b7bcae816729c211f9a5d4a1
botoberich <botoberich@gmail.com>
2019-08-31 10:51:28 -0400

    Refactor header with Match render prop

--- web/src/app/components/header.tsx
+++ web/src/app/components/header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, navigate } from 'gatsby';
-import { Location } from '@reach/router';
+import { Location, Match } from '@reach/router';
 
 // State
 import { Button, Layout, Icon, Avatar, Menu, Dropdown, Badge, Tag, notification, Typography, Tooltip } from 'antd';
@@ -13,9 +13,12 @@ import { usePhotoContext } from '../contexts/PhotoContext';
 import styles from './header.module.css';
 import { ArgsProps } from 'antd/lib/notification';
 
+// Types
+import { IMatchProps } from '../interfaces/ui.interface';
+
 const { Header } = Layout;
 const { Paragraph } = Typography;
-const notificationConfig = (msg: string): ArgsProps => ({
+const notificationCoinfig = (msg: string): ArgsProps => ({
     placement: 'bottomRight',
     bottom: 50,
     duration: 3,
@@ -34,14 +37,19 @@ function PageHeader() {
     };
     return (
         <Location>
-            {({ location }) => {
-                const pathParts = location.pathname.split('/').filter(str => str !== '');
-                const headerTitle = pathParts.length > 1 ? upperFirstLetter(pathParts[1]) : upperFirstLetter('photos');
-                // Quick fix to hide upload and delete buttons on non-Photo screen
-                const isPhotoScreen = location.pathname === '/app/';
+            {props => {
                 return (
                     <Header className={styles.header}>
-                        <span className={styles.headerTitle}>{headerTitle}</span>
+                        <Match path="/app/:title/*">
+                            {(props: IMatchProps) => {
+                                console.log({ props });
+                                return (
+                                    <span className={styles.headerTitle}>
+                                        {props.match ? props.match.title : 'photos'}
+                                    </span>
+                                );
+                            }}
+                        </Match>
                         <nav className={styles.nav}>
                             <div className={styles.navItem}>
                                 {selectedThumbnails.length > 0 && (
@@ -51,29 +59,65 @@ function PageHeader() {
                                     </Tag>
                                 )}
                             </div>
-                            {isPhotoScreen && (
-                                <div className={styles.navItem}>
-                                    <Upload setThumbnails={setThumbnails} progressDispatch={progressDispatch}></Upload>
-                                </div>
-                            )}
-                            {isPhotoScreen && (
-                                <div className={styles.navItem}>
-                                    <Delete
-                                        progressDispatch={progressDispatch}
-                                        selectedThumbnails={selectedThumbnails}
-                                        setSelectedThumbnails={setSelectedThumbnails}
-                                        setloadingThumbnails={setloadingThumbnails}
-                                        setThumbnails={setThumbnails}></Delete>
-                                </div>
-                            )}
-                            {isPhotoScreen && (
-                                <div className={styles.navItem}>
-                                    <Download
-                                        selectedThumbnails={selectedThumbnails}
-                                        setSelectedThumbnails={setSelectedThumbnails}
-                                    />
-                                </div>
-                            )}
+                            <Match path="/app/">
+                                {props =>
+                                    props.match && (
+                                        <div className={styles.navItem}>
+                                            <Upload
+                                                setThumbnails={setThumbnails}
+                                                progressDispatch={progressDispatch}></Upload>
+                                        </div>
+                                    )
+                                }
+                            </Match>
+                            <Match path="/app/">
+                                {props =>
+                                    props.match && (
+                                        <div className={styles.navItem}>
+                                            <Delete
+                                                progressDispatch={progressDispatch}
+                                                selectedThumbnails={selectedThumbnails}
+                                                setSelectedThumbnails={setSelectedThumbnails}
+                                                setloadingThumbnails={setloadingThumbnails}
+                                                setThumbnails={setThumbnails}></Delete>
+                                        </div>
+                                    )
+                                }
+                            </Match>
+                            <Match path="/app/">
+                                {props1 => (
+                                    <Match path="/app/albums/:id">
+                                        {props2 => {
+                                            if (props1.match || props2.match) {
+                                                return (
+                                                    <div className={styles.navItem}>
+                                                        <Download
+                                                            selectedThumbnails={selectedThumbnails}
+                                                            setSelectedThumbnails={setSelectedThumbnails}
+                                                        />
+                                                    </div>
+                                                );
+                                            }
+                                        }}
+                                    </Match>
+                                )}
+                            </Match>
+                            <Match path="/app/albums/:id">
+                                {(props: IMatchProps) => {
+                                    if (!props.match) {
+                                        return null;
+                                    }
+                                    return (
+                                        <div className={styles.navItem}>
+                                            <Remove
+                                                albumId={props.match.id}
+                                                selectedThumbnails={selectedThumbnails}
+                                                setSelectedThumbnails={setSelectedThumbnails}
+                                            />
+                                        </div>
+                                    );
+                                }}
+                            </Match>
                             <div className={styles.navItem} style={{ marginLeft: '25px' }}>
                                 <UserAvatar></UserAvatar>
                             </div>
@@ -182,6 +226,33 @@ function Download({ selectedThumbnails, setSelectedThumbnails }) {
                 }}>
                 <Icon type="copy" theme="twoTone" twoToneColor="#52c41a" />
                 <span className={styles.hideMobile}>Download</span>
+            </Button>
+        </Tooltip>
+    );
+}
+
+function Remove({ albumId, selectedThumbnails, setSelectedThumbnails }) {
+    return (
+        <Tooltip placement="bottom" title={selectedThumbnails.length === 0 ? 'Please select at least one photo.' : ''}>
+            <Button
+                disabled={selectedThumbnails.length === 0}
+                // onClick={e => {
+                //     try {
+                //         handleDownloadPhotos(selectedThumbnails);
+                //         notification.success(
+                //             notificationConfig(
+                //                 `Successfully downloaded ${selectedThumbnails.length > 1 ? 'files' : 'file'}.`
+                //             )
+                //         );
+                //     } catch (err) {
+                //         notification.error(notificationConfig(`Error downloading files. Please contact support.`));
+                //     }
+
+                //     setSelectedThumbnails([]);
+                // }}
+            >
+                <Icon type="copy" theme="twoTone" twoToneColor="#52c41a" />
+                <span className={styles.hideMobile}>Remove From Album</span>
             </Button>
         </Tooltip>
     );
--- web/src/app/interfaces/ui.interface.ts
+++ web/src/app/interfaces/ui.interface.ts
@@ -1,4 +1,16 @@
+import {  MatchRenderProps } from '@reach/router';
+
 export interface ProgressStartingPayload {
     length: number;
     cmd: 'Upload' | 'Delete' | 'Download' | 'Update';
+}
+
+
+interface IMatchParams {
+    id: string;
+    title: string;
+}
+
+export interface IMatchProps extends MatchRenderProps<IMatchParams> {
+    id?: string;
 }
