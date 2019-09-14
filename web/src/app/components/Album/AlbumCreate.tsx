170185a849100a6aff89b36bb13499bf90e1d9a5
botoberich <botoberich@gmail.com>
2019-09-14 01:36:09 -0400

    Remove unused stuff

--- web/src/app/components/Album/AlbumCreate.tsx
+++ web/src/app/components/Album/AlbumCreate.tsx
@@ -1,31 +1,22 @@
 import React from 'react';
-import { navigate } from 'gatsby';
 
 // UI
 import AlbumForm from './AlbumForm';
 import PhotoGrid, { usePhotoGrid } from '../Photo/PhotoGrid';
-import { Typography, Button, notification, Icon } from 'antd';
 import styles from './AlbumCreate.module.css';
 
 // State
 import { useSelectonContext } from '../../contexts/SelectionContext';
-import { createAlbum } from '../../services/album.service';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { skeletonSelector } from '../../redux/photo/photo.selectors';
 
 // Types
-import { ArgsProps } from 'antd/lib/notification';
-import { IAlbumMetadata } from '../../interfaces/albums.interface';
-import { notifySuccess } from '../../utils/notification';
-
-const { Title, Paragraph } = Typography;
 
 function AlbumCreate() {
     const { loading } = usePhotoGrid();
     const skeleton = useSelector(state => skeletonSelector(state));
 
-    const { selectedThumbnails, setSelectedThumbnails } = useSelectonContext();
-    const [creating, setCreating] = React.useState(false);
+    const { setSelectedThumbnails } = useSelectonContext();
 
     React.useEffect(() => {
         return () => {
